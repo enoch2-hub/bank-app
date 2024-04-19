c0fdc7fcd5635e27a911afec0f6600d3a9d4ba88
enoch2-hub <enoch.perera.coding2@gmail.com>
2024-04-19 22:23:06 +0530

    fix bz & onlineBz comp resp

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import Stats from './components/Stats/Stats'
-import Business from './components/Business/Business'
-import OnlineBz from './components/OnlineBz/OnlineBz'
-import Comments from './components/Comments/Comments'
-import CTA from './components/CTA/CTA'
-import Footer from './components/Footer/Footer'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Stats from "./components/Stats/Stats";
+import Business from "./components/Business/Business";
+import OnlineBz from "./components/OnlineBz/OnlineBz";
+import Comments from "./components/Comments/Comments";
+import CTA from "./components/CTA/CTA";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className=''>
-      </div>
-        <Navbar/>
-        <Hero/>
-        <Stats/>
-        <Business/>
-        <OnlineBz/>
-        <Comments/>
-        <CTA/>
-        <Footer/>
+      <div className=""></div>
+      <Navbar />
+      <Hero />
+      <Stats />
+      {/* <Business/> */}
+      {/* <OnlineBz/> */}
+      <Comments />
+      <CTA />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
