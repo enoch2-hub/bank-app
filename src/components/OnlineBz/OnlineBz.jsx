4501e1ac95cc2752ec0d9d8aaa167973eedfa707
enoch2-hub <enoch.perera.coding2@gmail.com>
2024-04-20 23:13:30 +0530

    resp fix 2

--- src/components/OnlineBz/OnlineBz.jsx
+++ src/components/OnlineBz/OnlineBz.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { onlinebz, onlinebz2 } from "../../assets/course-assets";
-
 import "./OnlineBz.css";
 
 const OnlineBz = () => {
@@ -17,12 +15,9 @@ const OnlineBz = () => {
             ex voluptatem atque, ab adipisci non. officiis expedita consequatur
             sint eos ab natus porro c officiis expedita consequatur sint eos ab
             natus porro c officiis expedita consequatur sint eos ab natus porro
-            c officiis expedita consequatur sint eos ab natus porro c officiis
-            expedita consequatur sint eos ab natus porro c officiis expedita
-            consequatur sint eos ab natus porro c
+
           </p>
           <div className="onlineBz-container-1-btn">Get Started</div>
-          {/* <div className="onlineBz-container-1-btn">Get Started</div> */}
         </div>
       </div>
 
@@ -41,24 +36,6 @@ const OnlineBz = () => {
           <img src={onlinebz2} alt="" />
         </div>
       </div>
-
-      {/* <div className="onlineBz-container-2">
-        <div className="onlineBz-container-2-text">
-            <h1>Mobile Business aliquam numquam nostrum</h1> 
-            <p>voluptatem atque, ab adipisci non.
-            officiis expedita consequatur sint eos ab natus porro c
-            officiis expedita consequatur sint eos ab natus porro c
-            officiis expedita consequatur sint eos ab natus porro c
-            officiis expedita consequatur sint eos ab natus porro c
-            officiis expedita consequatur sint eos ab natus porro c
-            officiis expedita consequatur sint eos ab natus porro c
-            </p>
-            <div className='onlineBz-container-2-btn'>Get Started</div>
-        </div>
-        <div className="onlineBz-container-2-img">
-            <img src={onlinebz2} alt="" />
-        </div>
-    </div> */}
     </>
   );
 };
